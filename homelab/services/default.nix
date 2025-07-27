1c5da6a13ff4ecb43507c50f086f9c5829f91a98
Willis Plummer <willisplummer@gmail.com>
2025-07-27 09:21:55 -0400

    working?

--- homelab/services/default.nix
+++ homelab/services/default.nix
@@ -14,12 +14,32 @@
       80
       443
     ];
+    security.acme = {
+      acceptTerms = true;
+      defaults.email = "willisplummer@gmail.com";
+      certs."nixos.local" = {
+        listenHTTP = "0.0.0.0:80"; # Provide a dummy HTTP listener for ACME
+        # Other necessary ACME options if applicable, but keep them minimal since ACME won't be actively used for this domain
+      };
+
+    };
     services.caddy = {
       enable = true;
       globalConfig = ''
         auto_https off
       '';
+      ca = null;
       virtualHosts = {
+        "http://${config.homelab.baseDomain}" = {
+          extraConfig = ''
+            redir https://{host}{uri}
+          '';
+        };
+        "http://*.${config.homelab.baseDomain}" = {
+          extraConfig = ''
+            redir https://{host}{uri}
+          '';
+        };
       };
     };
   };
